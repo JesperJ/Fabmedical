6fb8d8fd068b6b57fb324e36d0386a8eafead9fb
Jesper Jeansson <jesper.jeansson@kalmar.se>
2021-10-26 13:25:44 +0000

    Added Application Insights

--- content-web/app.js
+++ content-web/app.js
@@ -5,6 +5,10 @@ const request = require('request');
 
 const app = express();
 
+const appInsights = require("applicationinsights");
+appInsights.setup("7af8ee31-67ba-4115-92f9-4c45e6662a94");
+appInsights.start();
+
 app.use(express.static(path.join(__dirname, 'dist/content-web')));
 const contentApiUrl = process.env.CONTENT_API_URL || "http://52.186.147.76:3001";
 
